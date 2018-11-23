0e6fca37fdb35d91b41ff394e70d9cfd9a268c64
zhouliujun <1096432931@qq.com>
2018-11-23 22:19:56 +0800

    更新项目说明文档

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: `"development"`,
-    BASE_URL: `"/api"`
+    BASE_URL: `"/api"` //开发环境API地址
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
     NODE_ENV: `"production"`,
-    BASE_URL: `"http://192.168.0.220"`
+    BASE_URL: `"http://api.hao2013.cn"` //生产环境API地址
 }
