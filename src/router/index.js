b0297133661ac76def727d862c772c4a32f7c32c
zhouliujun <1096432931@qq.com>
2018-12-30 11:28:57 +0800

    移除debugger

--- src/router/index.js
+++ src/router/index.js
@@ -63,7 +63,6 @@ router.beforeEach((to, from, next) => {
         if (store.getters.token) { // 判断是否登录
             next();
         } else { // 没登录则跳转到登录界面
-            debugger
             window.localStorage.removeItem('adminSessionData');
             next({
                 path: '/login',
